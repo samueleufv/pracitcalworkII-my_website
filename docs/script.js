baef016c549ecb90ecc297c7ed987dbf78037cbc
samueleufv <9407732@alumnos.ufv.es>
2024-12-29 02:41:35 +0100

    adding catchpa control in contact.html

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,5 @@
+
+//function for sidebar in degree.html , is used to make appear or disappear the list with all the courses per year
 function toggleList(id) {
     const list = document.getElementById(id);
     if (list.classList.contains('hidden')) {
@@ -6,4 +8,28 @@ function toggleList(id) {
       list.classList.add('hidden');
     }
   }
+
+
+  //function for captcha in contact.html
+function generateCaptcha() {
+    const num1 = Math.floor(Math.random() * 10) + 1; 
+    const num2 = Math.floor(Math.random() * 10) + 1; 
+    const question = `${num1} + ${num2}`;
+    document.getElementById('captcha-question').innerText = question; 
+    return num1 + num2; 
   
+  
+}
+  let correctAnswer = generateCaptcha(); 
+  
+
+  document.querySelector('form').addEventListener('submit', function(event) {
+    const userAnswer = parseInt(document.getElementById('captcha').value, 10); 
+    if (userAnswer !== correctAnswer) { 
+      alert('Captcha non corretto. Riprova.'); 
+      event.preventDefault(); 
+      correctAnswer = generateCaptcha();
+    }
+  });
+
+
